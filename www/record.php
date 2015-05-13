2649d59d9fe527d58a332f05a594e9d177335b15
unknown <azizana@AZIZANA2.asiapacific.hpqcorp.net>
2015-05-13 22:05:44 +0800

    iGST project First Dev

---
+++ www/record.php
@@ -0,0 +1,19 @@
+{
+"records": [
+  {
+    "Name" : "MILO",
+    "Price" : "3.00",
+    "GST" : "Yes"
+  },
+  {
+    "Name" : "Horlick",
+    "Price" : "2.00",
+    "GST" : "Yes"
+  },
+  {
+    "Name" : "Adidas,
+    "Price" : "215.00",
+    "GST" : "No"
+  },
+]
+}
